a66b4436fd5a740da1427e464e9942695d40b06e
Didarul Shahriar <didarul13579@gmail.com>
2026-05-14 06:54:03 +0600

    Wrapped up the project with AuthProvider and added Protected Route

---
+++ code/frontend/src/app/(auth)/login/page.jsx
@@ -0,0 +1,7 @@
+export default function page() {
+    return (    
+        <div>
+            LOGIN PAGE
+        </div>
+    );
+}    
---
+++ code/frontend/src/components/shared/ProtectedRoute.jsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { useAuth } from "@/context/AuthProvider";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+const ProtectedRoute = ({children}) => {
+    const {user, loading} = useAuth();
+    const router = useRouter();
+
+    useEffect(() => {
+        if(!loading && !user){
+            router.push("/login");
+        }
+    }, [user, loading, router]);
+
+    if(loading){
+
+    }
+    
+    if(user){
+        return children;
+    }
+
+    return null;
+};
+
+export default ProtectedRoute;
