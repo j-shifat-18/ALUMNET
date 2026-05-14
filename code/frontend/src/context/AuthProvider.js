8ce4c883b7707fa7509948decceb4a2f9ad0e3d5
Didarul Shahriar <didarul13579@gmail.com>
2026-05-14 06:22:32 +0600

    added AuthProvider

---
+++ code/frontend/src/context/AuthProvider.js
@@ -0,0 +1,82 @@
+"use client"
+
+import { auth } from "@/lib/firebase";
+import { createUserWithEmailAndPassword, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
+import { createContext, useContext, useEffect, useState } from "react"
+
+const AuthContext = createContext();
+const googleProvider = new GoogleAuthProvider();
+
+export const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+            setUser(currentUser);
+            setLoading(false);
+        });
+
+        return () => unsubscribe();
+    }, []);
+
+    const registerUser = async (email, password) => {
+        setLoading(true);
+        try {
+            const result = await createUserWithEmailAndPassword(auth, email, password);
+
+            return result.user;
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    const signInUser = async (email, password) => {
+        setLoading(true);
+        try {
+            const result = await signInWithEmailAndPassword(auth, email, password);
+
+            return result.user;
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    const signInGoogle = async () => {
+        setLoading(true);
+        try {
+            const result = await signInWithPopup(auth, googleProvider);
+
+            return result.user;
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    const logout = async () => {
+        setLoading(true);
+        try {
+            await signOut(auth);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    const updateUserProfile = (profile) => {
+        return updateProfile(auth.currentUser, profile);
+    };
+
+    return (
+        <AuthContext.Provider
+            value={{
+                user, loading, registerUser, signInUser, signInGoogle, logout, updateUserProfile
+            }}
+        >
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export const useAuth = () => {
+    return useContext(AuthContext);
+};
