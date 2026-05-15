8b923b14cdd4caca982ce5360fca8e82a6f8bd98
j-shifat-18 <info.jahirulsifat@gmail.com>
2026-05-15 10:19:01 +0600

    backend : update prisma schema , add test server

---
+++ code/backend/src/lib/prisma.ts
@@ -0,0 +1,21 @@
+import { PrismaClient } from "../../generated/prisma/client.js";
+import { PrismaNeon } from "@prisma/adapter-neon";
+import "dotenv/config";
+
+const adapter = new PrismaNeon({
+  connectionString: process.env.DATABASE_URL!,
+});
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    adapter,
+  });
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
--- code/backend/src/server.ts
+++ code/backend/src/server.ts
@@ -1,1 +1,47 @@
-console.log(process.env.PORT);
+import express from "express";
+import { prisma } from './lib/prisma.js';
+import { Request, Response } from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req :Request, res : Response) => {
+  res.send("ALUMNET Backend Running");
+});
+
+app.post("/create-user", async (req, res) => {
+  try {
+    const user = await prisma.user.create({
+      data: {
+        name: "Shifat",
+        email: "shifat@example.com",
+      },
+    });
+
+    res.json(user);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Error creating user",
+    });
+  }
+});
+
+app.get("/users", async (req, res) => {
+  try {
+    const users = await prisma.user.findMany();
+
+    res.json(users);
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).json({
+      message: "Error fetching users",
+    });
+  }
+});
+
+app.listen(process.env.PORT || 3123, () => {
+  console.log("Server running on port " + (process.env.PORT || 3123));
+});
