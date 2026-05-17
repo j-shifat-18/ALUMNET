769b13620f09633dc9175f21bc316851700cc27f
j-shifat-18 <info.jahirulsifat@gmail.com>
2026-05-17 12:58:23 +0600

    backend : setup firebase admin sdk , add auth middleware , update cors config

--- code/backend/src/app/modules/user/user.route.ts
+++ code/backend/src/app/modules/user/user.route.ts
@@ -1,9 +1,10 @@
 import express from 'express';
 import { UserController } from "./user.controller.js";
+import { auth } from '../../middlewares/auth.js';
 
 const router = express.Router();
 
-router.get("/" , UserController.getAllUsers);
-router.post("/" , UserController.createUser);
+router.get("/", auth, UserController.getAllUsers);
+router.post("/",  UserController.createUser);
 
 export const UserRoutes = router;
